80ec3f858b35cd092377e7a05fd9194beead5d7d
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2023-12-15 14:34:46 +0100

    desktop works

--- src/app/page.js
+++ src/app/page.js
@@ -1,96 +1,57 @@
 import styles from './page.module.css'
+import Image from 'next/image';
+import { Favorite, FastRewind, PlayCircle, ArrowBackIos, MoreVert, AddCircle, FastForward} from '@mui/icons-material';
+
+function getCover() {
+  const covers = ["colorful", "demon", "drums", "future", "greyscale", "moon"];
+  return "/cover/" + covers[Math.floor(Math.random() * covers.length)] + ".jpg"
+}
+
+function MusicItem() {
+  const titles = ["Music", "Dream", "Epic", "Ballad", "Rock", "Classic", "Sky", "Future", "Apocalypse"];
+  const artists = ["The Bugs", "Man", "Super", "Punks", "Cyborg", "Fire", "Devils", "Salt", "Phones"];
+  
+
+  const generatedTitle = titles[Math.floor(Math.random() * titles.length)] 
+  + " " + titles[Math.floor(Math.random()*titles.length)];
+  const generatedArtist = artists[Math.floor(Math.random() * artists.length)] 
+  + " " + artists[Math.floor(Math.random() * artists.length)] 
+  + " " + artists[Math.floor(Math.random() * artists.length)];
+  return (
+    <div className={styles["info-item"]}>
+    <Image width={94} height={94} className={styles["info-image"]} src={getCover()} alt="A generic album cover" />
+    <div className={styles["info"]}>
+      <p className={styles["info-artist"]}>{generatedArtist}</p>
+      <p className={styles["info-title"]}>{generatedTitle}</p>
+    </div>
+    <span className={styles["material-icons"]}><PlayCircle /></span>
+  </div>
+  )
+}
 
 export default function Home() {
   return (
     <div>
       <nav className={styles["navbar"]}>
-        <span className={styles["material-icons"]}>arrow_back_ios_new</span>
+        <span className={styles["material-icons"]}><ArrowBackIos /></span>
         <p>Playlist - Generic Playlist (3)</p>
-        <span className={styles["material-icons"]}>more_vert</span>
+        <span className={styles["material-icons"]}><MoreVert /></span>
       </nav><div className={styles["root"]}>
         <aside className={styles["tracks"]}>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/drums.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Super Rockstar</p>
-              <p className={styles["info-title"]}>Crowd Apocalypse</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/colorful.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Colorful Musicman</p>
-              <p className={styles["info-title"]}>The color of P.A.I.N.</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/demon.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>John Smith</p>
-              <p className={styles["info-title"]}>Space Adventure</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/drums.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Super Rockstar</p>
-              <p className={styles["info-title"]}>Why is my face</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/future.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Music Person 3</p>
-              <p className={styles["info-title"]}>Giant Face</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/moon.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Dr. Warewolf</p>
-              <p className={styles["info-title"]}>Bright Moon</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/greyscale.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Really old person</p>
-              <p className={styles["info-title"]}>Like it used to be</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/colorful.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Colorful Musicman</p>
-              <p className={styles["info-title"]}>color (feat. Dr. Warewolf)</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
-          <div className={styles["info-item"]}>
-            <img className={styles["info-image"]} src="/assets/cover/moon.jpg" alt="Album cover" />
-            <div className={styles["info"]}>
-              <p className={styles["info-artist"]}>Dr. Warewolf</p>
-              <p className={styles["info-title"]}>Really bright moon II</p>
-            </div>
-            <span className={styles["material-icons"]}>play_circle</span>
-          </div>
+            {Array(20).fill(1).map(key => (
+            <MusicItem key={key}/>
+          ))}
+          
         </aside>
         <main className={styles["player"]}>
-          <img className={styles["album-image"]} src="/assets/cover/drums.jpg" alt="A generic album cover" />
+          <Image width={512} height={512} className={styles["album-image"]} src={getCover()} alt="A generic album cover" />
           <div className={styles["player-info"]}>
-            <span className={styles["material-icons"]}>add_circle</span>
+            <span className={styles["material-icons"]}><AddCircle /></span>
             <div>
               <p className={styles["info-artist"]}>Super Rockstar</p>
               <p className={styles["info-title"]}>Crowd Apocalypse</p>
             </div>
-            <span className={styles["material-icons"]}>favorite</span>
+            <span className={styles["material-icons"]}><Favorite /></span>
           </div>
           <div className={styles["seekbar"]}>
             <p>0.40</p>
@@ -98,13 +59,13 @@ export default function Home() {
             <p>3.42</p>
           </div>
           <div className={styles["controls"]}>
-            <span className={styles["material-icons"]}>repeat</span>
+            <span className={styles["material-icons"]}><repeat /></span>
             <div className={styles["center-buttons"]}>
-              <span className={styles["material-icons"]}>fast_rewind</span>
-              <span className={styles["material-icons"]}>play_circle</span>
-              <span className={styles["material-icons"]}>fast_forward</span>
+              <span className={styles["material-icons"]}><FastRewind /></span>
+              <span className={styles["material-icons"] + " " + styles["play"] }><PlayCircle /></span>
+              <span className={styles["material-icons"]}><FastForward /></span>
             </div>
-            <span className={styles["material-icons"]}>shuffle</span>
+            <span className={styles["material-icons"]}><shuffle /></span>
           </div>
         </main>
       </div>
